d02fc5fb50a3d2e1c66a1fd3d74452fcd67ea4a2
hardik302001 <56587994+hardik302001@users.noreply.github.com>
2020-10-01 01:02:52 +0530

    Update heapsort.py

--- Sorting/heapsort.py
+++ Sorting/heapsort.py
@@ -1,3 +1,30 @@
+'''
+
+Example of HEAPSORT.
+Suppose we have array as 4,3,7,1,8,5 initially.
+So heap sort will be applied as follows:
+
+
+
+
+Step 1: 8 is swapped with 5.
+Step 2: 8 is disconnected from heap as 8 is in correct position now and.
+Step 3: Max-heap is created and 7 is swapped with 3.
+Step 4: 7 is disconnected from heap.
+Step 5: Max heap is created and 5 is swapped with 1.
+Step 6: 5 is disconnected from heap.
+Step 7: Max heap is created and 4 is swapped with 3.
+Step 8: 4 is disconnected from heap.
+Step 9: Max heap is created and 3 is swapped with 1.
+Step 10: 3 is disconnected.
+
+
+'''
+
+
+
+
+
 def heapify(nums, heap_size, root_index):
     # Assume the index of the largest element is the root index
     largest = root_index
